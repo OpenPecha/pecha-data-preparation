c511e9b1f198fca1262aa4e43370215b8bb9fa2b
drupchen <hhdrupchen@gmail.com>
2024-04-24 22:34:19 +0100

    create stubs of exporting structure to nested folders

--- preparation/__init__.py
+++ preparation/__init__.py
@@ -1,1 +1,2 @@
 from .parse_data import parse_data_csv
+from .export_2_folders import export_2_folders
--- usage.py
+++ usage.py
@@ -7,8 +7,13 @@ in_path = Path('input')
 out_path = Path('output')
 
 for f in in_path.glob('*.csv'):
+    # parse file
     struct = parse_data_csv(f)
+
+    # export to json
     dump = json.dumps(struct, indent=4, ensure_ascii=False)
     out_file = out_path / f'{f.stem}.json'
     out_file.write_text(dump)
-    print()
+
+    # export to folder structure
+    export_2_folders(struct, out_path)
