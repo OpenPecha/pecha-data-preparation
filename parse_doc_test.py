a8a94bdd5b987b03d403f1d862482c5bbc38218a
drupchen <hhdrupchen@gmail.com>
2024-06-17 22:41:02 +0100

    add input output files

---
+++ parse_doc_test.py
@@ -0,0 +1,6 @@
+from preparation.single_doc_manual_prep import *
+
+
+in_file = 'input/drive/RAF75A533 སྒྲོལ་མའི་འགྲེལ་བ་འཕྲིན་ལས་ཆར་དུ་སྙིལ་བའི་སྤྲིན་ཕུང་། Segmentation - corrected.docx'
+
+parse_doc(in_file)
